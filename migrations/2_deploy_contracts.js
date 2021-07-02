8eb42bc781dd49b3a73cfc953ba34a7294c9a750
leang27 <52003343+leang27@users.noreply.github.com>
2021-07-02 17:14:08 +0700

    delete test token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,5 @@
 const Factory = artifacts.require("BitrielFactory");
-const Kum = artifacts.require("Kum");
-const Sel = artifacts.require("Sel");
 
 module.exports = async function (deployer) {
-  await Promise.all([
-    deployer.deploy(Sel),
-    deployer.deploy(Kum),
-    deployer.deploy(Factory)
-  ]);
-  const [sel, kum, factory] = await Promise.all([
-    Sel.deployed(),
-    Kum.deployed(),
-    Factory.deployed()
-  ]);
-  await factory.createPool(kum.address, sel.address, 3000);
+  await deployer.deploy(Factory);
 };
